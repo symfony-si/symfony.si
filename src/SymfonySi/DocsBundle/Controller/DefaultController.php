a155328a98e73b17c81a4c3a0bb5d4382f944eed
Peter Kokot <peterkokot@gmail.com>
2013-10-09 22:17:38 +0200

    documentation page updated

--- src/SymfonySi/DocsBundle/Controller/DefaultController.php
+++ src/SymfonySi/DocsBundle/Controller/DefaultController.php
@@ -8,6 +8,25 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('SymfonySiDocsBundle:Default:index.html.twig');
+        $book = round(0/21 * 100);
+        $bundles = round(0/2 * 100);
+        $components = round(0/83 * 100);
+        $contributing = round(0/21 * 100);
+        $cookbook = round(0/130 * 100);
+        $quickTour = round(0/5 * 100);
+        $reference = round(0/140 * 100) ;
+        $others = round(5/5 * 100);
+        $sum = round(5/407 * 100);
+        return $this->render('SymfonySiDocsBundle:Default:index.html.twig', array(
+            'book' => $book . ' %',
+            'bundles' => $bundles . ' %',
+            'components' => $components . ' %',
+            'contributing' => $contributing . ' %',
+            'cookbook' => $cookbook . ' %',
+            'quicktour' => $quickTour . ' %',
+            'reference' => $reference . ' %',
+            'others' => $others . ' %',
+            'sum' => $sum . ' %'
+        ));
     }
 }
