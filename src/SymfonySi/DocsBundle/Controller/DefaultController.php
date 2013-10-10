b0646d7668e257ea6b8ca2e6c6dfdfac44bbfb61
Peter Kokot <peterkokot@gmail.com>
2013-10-10 17:01:23 +0200

    documentation bundle num of files updated

--- src/SymfonySi/DocsBundle/Controller/DefaultController.php
+++ src/SymfonySi/DocsBundle/Controller/DefaultController.php
@@ -12,11 +12,11 @@ class DefaultController extends Controller
         $bundles = round(2/2 * 100);
         $components = round(0/83 * 100);
         $contributing = round(0/21 * 100);
-        $cookbook = round(0/130 * 100);
+        $cookbook = round(0/131 * 100);
         $quickTour = round(3/5 * 100);
         $reference = round(0/140 * 100) ;
         $others = round(5/5 * 100);
-        $sum = round(10/407 * 100);
+        $sum = round(10/408 * 100);
         return $this->render('SymfonySiDocsBundle:Default:index.html.twig', array(
             'book' => $book . ' %',
             'bundles' => $bundles . ' %',
